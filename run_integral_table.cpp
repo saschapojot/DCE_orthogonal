7703e7cbcec6f4d6b72c23ed48b77f87e040ab72
Xi LIU <xi.liu@u.nus.edu>
2025-12-09 20:13:26 +0800

    gsl

--- run_integral_table.cpp
+++ run_integral_table.cpp
@@ -1,10 +1,23 @@
 #include "./compute_integral_table/integral_table.hpp"
 
+
+
 int main(int argc, char *argv[])
 {
     if (argc != 2) {
         std::cout << "wrong arguments" << std::endl;
         std::exit(2);
     }
+    auto table_obj=table(std::string(argv[1]));
+
+
+
+    double a = 2.0;
+    double x = 3.0;
+
+    double rst=table_obj.pcf_U(a,x);
+    std::cout<<rst<<std::endl;
+
+
 
 }
